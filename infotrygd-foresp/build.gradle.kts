a293c8756b8c2166b7ffa71fe4c972b25e1ea9dd
Kevin Sillerud <kevin.sillerud@gmail.com>
2018-11-29 12:55:56 +0100

    Use JSR310 for infotrygd-foresp

--- infotrygd-foresp/build.gradle.kts
+++ infotrygd-foresp/build.gradle.kts
@@ -3,11 +3,13 @@ tasks {
     val generateJaxb2 by tasks.creating(Jaxb2Task::class) {
         outputDir = ext["xjbOutputDir"] as File
         sourceDir = file("$projectDir/src/main/xsd")
+        xjbDir = file("$projectDir/src/main/xjb")
         config = listOf(
                 Jaxb2Config(
                         targetPackage = "no.nav.helse.infotrygd.foresp",
                         schema = "RTV_IT_IFT.v14.xsd",
-                        encoding = "UTF-8"
+                        encoding = "UTF-8",
+                        binding = "binding.xml"
                 )
         )
     }

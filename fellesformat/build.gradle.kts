e8f067db7314f02ccd796ecdacfddd785438b941
Kevin Sillerud <kevin.sillerud@gmail.com>
2018-11-29 13:11:50 +0100

    Use JSR310 for fellesformat

--- fellesformat/build.gradle.kts
+++ fellesformat/build.gradle.kts
@@ -1,12 +1,16 @@
+version = "1.1-SNAPSHOT"
+
 tasks {
     val generateJaxb2 by tasks.creating(Jaxb2Task::class) {
         outputDir = ext["xjbOutputDir"] as File
         sourceDir = file("$projectDir/src/main/xsd")
+        xjbDir = file("$projectDir/src/main/xjb")
         config = listOf(
                 Jaxb2Config(
                         targetPackage = "no.nav.helse.sm2013",
                         schema = "NAV_Fellesformat-v1_eia2.xsd",
-                        encoding = "UTF-8"
+                        encoding = "UTF-8",
+                        binding = "binding.xml"
                 )
         )
     }

50b48ac3c8d36c1d28c94309b6534db1eb4fb7f9
Joakim kartveit <joakim.kartveit@nav.no>
2018-12-19 14:36:15 +0100

    La til xjb binding på oppforlingsplan

--- oppfolgingsplan/build.gradle.kts
+++ oppfolgingsplan/build.gradle.kts
@@ -2,21 +2,25 @@ tasks {
     val generateJaxb2 by tasks.creating(Jaxb2Task::class) {
         outputDir = ext["xjbOutputDir"] as File
         sourceDir = file("$projectDir/src/main/xsd")
+        xjbDir = file("$projectDir/src/main/xjb")
         config = listOf(
                 Jaxb2Config(
                         targetPackage = "no.nav.helse.op2012",
                         schema = "Oppfolgingsplan_Altinn.xsd",
-                        encoding = "UTF-8"
+                        encoding = "UTF-8",
+                        binding = "binding.xml"
                 ),
                 Jaxb2Config(
                         targetPackage = "no.nav.helse.op2014",
                         schema = "Oppfolgingsplan2_4M.xsd",
-                        encoding = "UTF-8"
+                        encoding = "UTF-8",
+                        binding = "binding.xml"
                 ),
                 Jaxb2Config(
                         targetPackage = "no.nav.helse.op2016",
                         schema = "Oppfolgingsplan201607_2.xsd",
-                        encoding = "UTF-8"
+                        encoding = "UTF-8",
+                        binding = "binding.xml"
                 )
         )
     }

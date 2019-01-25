acea73d5312ebf33bb10151dc09089114f808c61
Joakim kartveit <joakim.kartveit@nav.no>
2019-01-25 12:23:52 +0100

    La til binding fil for å lette forholde oss til datoer

--- arenaSykmelding-1/build.gradle.kts
+++ arenaSykmelding-1/build.gradle.kts
@@ -1,18 +1,21 @@
-version = "1.0.1-SNAPSHOT"
+version = "1.0.2-SNAPSHOT"
 tasks {
     val generateJaxb2 by tasks.creating(Jaxb2Task::class) {
         outputDir = ext["xjbOutputDir"] as File
         sourceDir = file("$projectDir/src/main/xsd")
+        xjbDir = file("$projectDir/src/main/xjb")
         config = listOf(
                 Jaxb2Config(
                         targetPackage = "no.nav.helse.arenaSykemelding",
                         schema = "ArenaSykmelding_1.xsd",
-                        encoding = "UTF-8"
+                        encoding = "UTF-8",
+                        binding = "binding.xml"
                 ),
                 Jaxb2Config(
                         targetPackage = "no.nav.helse.eiaDokumentInfo",
                         schema = "EiaDokumentInfo1-0.xsd",
-                        encoding = "UTF-8"
+                        encoding = "UTF-8",
+                        binding = "binding.xml"
                 )
         )
     }

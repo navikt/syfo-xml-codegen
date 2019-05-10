563ae46730c7bc653fea092cb8fdbe2a4759c628
Joakim kartveit <joakim.kartveit@nav.no>
2019-05-10 09:41:18 +0200

    La til kontrollsystemblokk, for infotrygdblokk uten namespace

---
+++ kontrollsystemblokk/build.gradle.kts
@@ -0,0 +1,22 @@
+version = "1.0-SNAPSHOT"
+
+tasks {
+    val generateJaxb2 by tasks.creating(Jaxb2Task::class) {
+        outputDir = ext["xjbOutputDir"] as File
+        sourceDir = file("$projectDir/src/main/xsd")
+        xjbDir = file("$projectDir/src/main/xjb")
+        config = listOf(
+                Jaxb2Config(
+                        targetPackage = "no.nav.helse.sm2013",
+                        schema = "kontrollSystemBlokk.xsd",
+                        encoding = "UTF-8",
+                        binding = "binding.xml"
+                )
+        )
+    }
+
+    "compileJava" {
+        dependsOn(generateJaxb2)
+    }
+}
+

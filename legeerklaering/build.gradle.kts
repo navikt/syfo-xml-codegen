f5426e4afa13ed8e1de3e44c6f2701f6c689e0bc
Joakim kartveit <joakim.kartveit@nav.no>
2019-05-16 08:59:39 +0200

    La ny versjon av legeerklaering skjema

--- legeerklaering/build.gradle.kts
+++ legeerklaering/build.gradle.kts
@@ -1,10 +1,12 @@
+version = "1.0-SNAPSHOT"
+
 tasks {
     val generateJaxb2 by tasks.creating(Jaxb2Task::class) {
         outputDir = ext["xjbOutputDir"] as File
         sourceDir = file("$projectDir/src/main/xsd")
         config = listOf(
                 Jaxb2Config(
-                        targetPackage = "no.nav.helse.sm2013",
+                        targetPackage = "no.nav.helse.legeerklaering",
                         schema = "NAV-Legeerklaring-2008-06-06.xsd",
                         encoding = "UTF-8"
                 )

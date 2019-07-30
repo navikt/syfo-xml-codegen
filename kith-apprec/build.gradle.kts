2f029fcd81e3566d03d2c84e224e1a9e94738b6c
Joakim Kartveit <joakimkartveit@gmail.com>
2019-07-30 14:29:12 +0200

    Legger til kith-apprec

---
+++ kith-apprec/build.gradle.kts
@@ -0,0 +1,20 @@
+tasks {
+    val generateJaxb2 by creating(Jaxb2Task::class) {
+        System.setProperty("javax.xml.accessExternalSchema", "all")
+        outputDir = ext["xjbOutputDir"] as File
+        sourceDir = file("$projectDir/src/main/xsd")
+        xjbDir = file("$projectDir/src/main/xjb")
+        config = listOf(
+                Jaxb2Config(
+                        targetPackage = "no.nav.helse.apprec",
+                        schema = "AppRec-v1.1.xsd",
+                        encoding = "UTF-8",
+                        binding = "binding.xml"
+                )
+        )
+    }
+
+    withType<JavaCompile> {
+        dependsOn(generateJaxb2)
+    }
+}

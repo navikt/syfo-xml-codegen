5c924ef4f04022bbb850aaf299eb8e4464c1ca6a
Joakim Kartveit <joakimkartveit@gmail.com>
2019-07-30 12:25:20 +0200

    La til xjb på xmlFellesformat

--- xmlfellesformat/build.gradle.kts
+++ xmlfellesformat/build.gradle.kts
@@ -2,11 +2,13 @@ tasks {
     val generateJaxb2 by creating(Jaxb2Task::class) {
         outputDir = ext["xjbOutputDir"] as File
         sourceDir = file("$projectDir/src/main/xsd")
+        xjbDir = file("$projectDir/src/main/xjb")
         config = listOf(
                 Jaxb2Config(
                         targetPackage = "no.nav.helse.eiFellesformat",
                         schema = "Fellesformat_any.xsd",
-                        encoding = "UTF-8"
+                        encoding = "UTF-8",
+                        binding = "binding.xml"
                 )
         )
     }

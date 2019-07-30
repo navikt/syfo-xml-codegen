81a251c8d47cd6fb05b59d46e2746823ac3ea7d0
Joakim Kartveit <joakimkartveit@gmail.com>
2019-07-30 16:19:47 +0200

    Går tilbake til apprec 1.0

--- kith-apprec/build.gradle.kts
+++ kith-apprec/build.gradle.kts
@@ -1,19 +1,12 @@
 tasks {
     val generateJaxb2 by creating(Jaxb2Task::class) {
-        System.setProperty("javax.xml.accessExternalSchema", "all")
         outputDir = ext["xjbOutputDir"] as File
         sourceDir = file("$projectDir/src/main/xsd")
         xjbDir = file("$projectDir/src/main/xjb")
         config = listOf(
                 Jaxb2Config(
-                        targetPackage = "no.nav.helse.apprecV10",
+                        targetPackage = "no.nav.helse.apprecV1-0",
                         schema = "AppRec-v1.0.xsd",
-                        encoding = "UTF-8",
-                        binding = "binding.xml"
-                ),
-                Jaxb2Config(
-                        targetPackage = "no.nav.helse.apprecV11",
-                        schema = "AppRec-v1.1.xsd",
                         encoding = "UTF-8",
                         binding = "binding.xml"
                 )

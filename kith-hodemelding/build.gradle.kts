4d73a033d743557ff42bdd3a2fd7a1bc66e31611
Joakim Kartveit <joakimkartveit@gmail.com>
2019-07-30 09:55:38 +0200

    Bytter encoding til ISO-8859-1

--- kith-hodemelding/build.gradle.kts
+++ kith-hodemelding/build.gradle.kts
@@ -8,7 +8,7 @@ tasks {
                 Jaxb2Config(
                         targetPackage = "no.nav.helse.xmlMsgHead",
                         schema = "xsd/MsgHead-v1_2.xsd",
-                        encoding = "UTF-8",
+                        encoding = "ISO-8859-1",
                         binding = "binding.xml"
                 )
         )

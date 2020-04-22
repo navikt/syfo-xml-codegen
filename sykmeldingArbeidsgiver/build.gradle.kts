2d95d41cfa05e7c8e1420836e00fdc5969f715cf
andreasnilsen <danduras@gmail.com>
2020-04-22 14:29:30 +0200

    Add sykmeldingArbeidsgiver

---
+++ sykmeldingArbeidsgiver/build.gradle.kts
@@ -0,0 +1,19 @@
+tasks {
+    val generateJaxb2 by creating(Jaxb2Task::class) {
+        outputDir = ext["xjbOutputDir"] as File
+        sourceDir = file("$projectDir/src/main/xsd")
+        xjbDir = file("$projectDir/src/main/xjb")
+        config = listOf(
+                Jaxb2Config(
+                        targetPackage = "no.nav.melding.virksomhet.sykmeldingarbeidsgiver",
+                        schema = "sykmeldingArbeidsgiver.xsd",
+                        encoding = "UTF-8",
+                        binding = "binding.xjb"
+                )
+        )
+    }
+    withType<JavaCompile> {
+        dependsOn(generateJaxb2)
+    }
+}
+
